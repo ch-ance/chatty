e7d0042dee276c990e81e01b422167cac49ee966
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-04 16:51:28 -0500

    incorporating friendId functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,18 +5,33 @@ import requiresConnection from "./HOCs/requiresConnection";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
+  }
+
+  addMessage = message => {
+    this.setState(prevState => ({
+      messages: [...prevState.messages, message]
+    }));
+  };
+
   render() {
     return (
       <div className="App">
-        <HomeScreen ws={this.props.ws} />
+        <HomeScreen
+          ws={this.props.ws}
+          messages={this.state.messages}
+          addMessage={this.addMessage}
+        />
       </div>
     );
   }
 }
 
-const HomeScreen = ({ ws }) => {
-  console.log(ws);
-
+const HomeScreen = ({ ws, messages, addMessage }) => {
   useEffect(() => {
     ws.onopen = () => {
       console.log("connected");
@@ -25,17 +40,26 @@ const HomeScreen = ({ ws }) => {
     ws.onmessage = evt => {
       const message = JSON.parse(evt.data);
       console.log("New Message: ", message);
+      addMessage(message);
     };
   }, []);
+
+  const [user_id, setUserId] = useState(0);
+
+  function getID() {
+    setUserId(Math.floor(Math.random() * 100));
+  }
   return (
     <div>
       <h1>HomeScreen</h1>
-      <Chat ws={ws} />
+      <Chat ws={ws} messages={messages} />
+      <button onClick={getID}>Get my ID</button>
+      <div>My unique id is: {user_id}</div>
     </div>
   );
 };
 
-const Chat = ({ ws }) => {
+const Chat = ({ ws, messages }) => {
   function sendMessage(event) {
     event.preventDefault();
     const message = { name: "Chance", message: messageText };
@@ -45,16 +69,30 @@ const Chat = ({ ws }) => {
 
   const [messageText, setMessageText] = useState("");
 
+  const [friendID, setFriendID] = useState(0);
+
   return (
     <div>
       <form onSubmit={sendMessage}>
+        <h3>Recipient ID: </h3>
+        <input
+          type="text"
+          value={friendID}
+          onChange={e => setFriendID(e.target.value)}
+        />
         <input
           type="text"
           value={messageText}
           onChange={e => setMessageText(e.target.value)}
         />
+
         <button>Send message</button>
       </form>
+      <ul>
+        {messages.map(message => {
+          return <li>{message.message}</li>;
+        })}
+      </ul>
     </div>
   );
 };
