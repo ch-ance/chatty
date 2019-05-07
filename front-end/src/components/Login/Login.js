fd0eddca2baddf87170a14fc569dfd7ca960b61a
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-06 23:23:33 -0500

    working on http requests to users db

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,13 +1,55 @@
 import React, { useState } from "react";
 
+import axios from "axios";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [registering, setRegistering] = useState(false);
+
+  const logIn = event => {
+    event.preventDefault();
+
+    axios.post(process.env.REACT_APP_USERS_DATABASE, {
+      username,
+      password
+    });
+  };
+
+  const register = event => {
+    event.preventDefault();
+
+    axios
+      .post(`${process.env.REACT_APP_USERS_DATABASE}/api/auth/register`, {
+        username,
+        password
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   return (
     <div className="login-page">
-      <h2>Login to Chatty</h2>
-      <form>
+      <div>
+        <button
+          className={!registering && "activeTab"}
+          onClick={() => setRegistering(false)}
+        >
+          Login
+        </button>
+        <button
+          className={registering && "activeTab"}
+          onClick={() => setRegistering(true)}
+        >
+          Sign Up
+        </button>
+      </div>
+
+      <h2>Chatty</h2>
+      <form onSubmit={registering ? logIn : register}>
         <span>Username: </span>
         <input
           type="text"
@@ -21,6 +63,8 @@ const Login = () => {
           value={password}
           onChange={e => setPassword(e.target.value)}
         />
+        <br />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
