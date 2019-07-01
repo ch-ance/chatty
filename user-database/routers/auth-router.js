b37baccf10787ff93b1ad681b243886555093040
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-30 19:29:04 -0500

    responds with a JWT when user logs in

--- user-database/routers/auth-router.js
+++ user-database/routers/auth-router.js
@@ -1,33 +1,45 @@
-const authRouter = require("express").Router();
-const Users = require("../data/helpers/users-model");
+const authRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const Users = require('../data/helpers/users-model')
 
-authRouter.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+authRouter.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body
 
-    const userID = Math.floor(Math.random() * 1000);
+        const userID = Math.floor(Math.random() * 1000)
 
-    const user = {
-      username,
-      password,
-      userID
-    };
-    await Users.add(user);
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(400).json({ message: "Error adding user" });
-  }
-});
+        const user = {
+            username,
+            password,
+            userID
+        }
+        await Users.add(user)
+        res.status(201).json({ user })
+    } catch (error) {
+        res.status(400).json({ message: 'Error adding user' })
+    }
+})
 
-authRouter.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+authRouter.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body
 
-    const [user] = await Users.findByUsername(username);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "User not found" });
-  }
-});
+        const [user] = await Users.findByUsername(username)
 
-module.exports = authRouter;
+        const hash = bcrypt.hashSync(user.password, 10)
+
+        const token = genToken(user)
+        res.status(201).json(
+            {
+                username: user.username,
+                password: hash
+            },
+            token
+        )
+    } catch (error) {
+        res.status(500).json({ message: 'User not found' })
+    }
+})
+
+module.exports = authRouter
