de647b321aae3c3fd4a69c282326e0c2ff5d38ad
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-02 17:14:55 -0500

    Adds MUI dependency, switching to dev environment

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,82 +1,82 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import axios from "axios";
-require("now-env");
+import axios from 'axios'
+require('now-env')
 const Login = ({ login }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [registering, setRegistering] = useState(false);
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [registering, setRegistering] = useState(false)
 
-  return (
-    <div className="login-page">
-      <div className="tab-selector">
-        <button
-          className={!registering ? "activeTab" : ""}
-          onClick={() => setRegistering(false)}
-        >
-          Login
-        </button>
-        <button
-          className={registering ? "activeTab" : ""}
-          onClick={() => setRegistering(true)}
-        >
-          Sign Up
-        </button>
-      </div>
+    return (
+        <div className="login-page">
+            <div className="tab-selector">
+                <button
+                    className={!registering ? 'activeTab' : ''}
+                    onClick={() => setRegistering(false)}
+                >
+                    Login
+                </button>
+                <button
+                    className={registering ? 'activeTab' : ''}
+                    onClick={() => setRegistering(true)}
+                >
+                    Sign Up
+                </button>
+            </div>
 
-      <h2>Chatty</h2>
-      <form onSubmit={registering ? register : logIn}>
-        <span>Username: </span>
-        <input
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <br />
-        <span>Password:</span>
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+            <h2>Chatty</h2>
+            <form onSubmit={registering ? register : logIn}>
+                <span>Username: </span>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <br />
+                <span>Password:</span>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 
-  function logIn(event) {
-    event.preventDefault();
+    function logIn(event) {
+        event.preventDefault()
 
-    axios
-      .post(`https://chatty-user-db.herokuapp.com/api/auth/login`, {
-        username,
-        password
-      })
-      .then(res => {
-        localStorage.setItem("userID", res.data.userID);
-        login(res.data);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
+        axios
+            .post(`${process.env.REACT_APP_USERS_DB}/api/auth/login`, {
+                username,
+                password
+            })
+            .then(res => {
+                localStorage.setItem('userID', res.data.userID)
+                login(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
 
-  function register(event) {
-    event.preventDefault();
+    function register(event) {
+        event.preventDefault()
 
-    axios
-      .post(`https://chatty-user-db.herokuapp.com/api/auth/register`, {
-        username,
-        password
-      })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
-};
+        axios
+            .post(`${process.env.REACT_APP_USERS_DB}/api/auth/register`, {
+                username,
+                password
+            })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+}
 
-export default Login;
+export default Login
