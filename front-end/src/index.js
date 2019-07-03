2bda57ff78a157050a4d4eb165fe35a6e71f00e3
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-02 19:43:05 -0500

    adds CSS Baseline to prevent inconsistencies across browsers

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.scss";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import './index.scss'
+import App from './App'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+    <Router>
+        <CssBaseline>
+            <App />
+        </CssBaseline>
+    </Router>,
+    document.getElementById('root')
+)
