fae98cc2ac20c87572eed0f96ee391d4a525fdf8
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-06 17:07:24 -0500

    adds nav bar to top of app

--- front-end/src/components/TopNav/index.js
+++ front-end/src/components/TopNav/index.js
@@ -4,18 +4,48 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import InputBase from '@material-ui/core/InputBase'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, fade } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu'
 import SearchIcon from '@material-ui/icons/Search'
 
-const useStyles = makeStyles()
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        border: '5px dotted black',
+        height: '8vh',
+    },
+    appBar: {
+        height: '100%',
+    },
+    menuButton: {
+        marginRight: '2rem',
+    },
+    title: {
+        fontSize: '2rem',
+        // display: 'block',
+    },
+    search: {
+        position: 'relative',
+        borderRadius: '2%',
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: '1rem',
+            width: 'auto',
+        },
+    },
+}))
 
 export default function SearchAppBar() {
     const classes = useStyles()
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.appBar}>
                 <Toolbar>
                     <IconButton
                         edge="start"
